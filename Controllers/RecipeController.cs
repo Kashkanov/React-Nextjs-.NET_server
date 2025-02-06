3f8dd587b0991854c93bccfae5d2be0e80a0604a
Kashkanov <aquinokarl17@gmail.com>
2025-02-06 17:49:23 +0800

    initial push

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRecipes()
+        {
+            var recipes = new List<string> { "Pasta", "Pizza", "Salad" };
+            return Ok(recipes);
+        }
+    }
+}
